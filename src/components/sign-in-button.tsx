eccaef7e26ae122aa88a05fbc1a265ceff0691b9
Cameron Matheson <cameron@workos.com>
2024-07-24 17:31:35 -0600

    borrow next-authkit-example's structure

---
+++ src/components/sign-in-button.tsx
@@ -0,0 +1,27 @@
+import { Button, Flex } from "@radix-ui/themes";
+import { useAuth } from "@workos-inc/authkit-react";
+
+export function SignInButton({ large }: { large?: boolean }) {
+  const { user, signIn, signOut } = useAuth();
+
+  if (user) {
+    return (
+      <Flex gap="3">
+        <Button onClick={signOut} size={large ? "3" : "2"}>
+          Sign Out
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Button
+      onClick={() => {
+        signIn();
+      }}
+      size={large ? "3" : "2"}
+    >
+      Sign In {large && "with AuthKit"}
+    </Button>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Layout from './Layout.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
 )
